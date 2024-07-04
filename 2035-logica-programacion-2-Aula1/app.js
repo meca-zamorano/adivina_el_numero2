f49fb7a789bc8474d7ab681c546ccca97a773d76
meca-zamorano <eimycabrerazamorano@gmail.com>
2024-07-03 22:04:55 -0500

    Se crea una función que permite asignar texto a los elementos html

--- 2035-logica-programacion-2-Aula1/app.js
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -1,11 +1,18 @@
-//AL H1 SE LE ASIGNA UNA VARIABLE, CON EL QUERYSELECTOR SE SELECCIONAL LA ETIQUETA QUE VAMOS A USAR
-let titulo = document.querySelector('h1');
+/*AL ELEMENTO P SE LE ASIGNA UNA VARIABLE, CON EL QUERYSELECTOR SE SELECCIONAL LA ETIQUETA QUE VAMOS A USAR
 let parrafo = document.querySelector('p');
-//USANDO LA VARIABLE Y CON EL INNERHTML SE LE ASIGNA UN TEXTO O VALOR A LA VARIABLE
-titulo.innerHTML = 'Juego del número secreto';
-parrafo.innerHTML = 'Indica un número del 1 al 10';
+USANDO LA VARIABLE Y CON EL INNERHTML SE LE ASIGNA UN TEXTO O VALOR A LA VARIABLE
+parrafo.innerHTML = 'Indica un número del 1 al 10';*/
 
 //CREAR FUNCIONES
+function asignarTextoElemento(elemento, texto){
+    let elementoHtml = document.querySelector(elemento);
+    elementoHtml.innerHTML = texto;
+}
+
 function intentoDeUsuario(){
     
 }
+
+//LLAMAR LAS FUNCIONES
+asignarTextoElemento('h1', 'Juego del número secreto');
+asignarTextoElemento('p', 'Indica un número del 1 al 10')
